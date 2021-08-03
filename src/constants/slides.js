29598ac43080951eb2c9128a805e260a6061b69b
fdavison1 <fred.noreply@gmail.com>
2021-08-03 18:46:10 -0500

    formatting

--- src/constants/slides.js
+++ src/constants/slides.js
@@ -1,14 +1,14 @@
-const commits = 2;
+const commits = 3;
 
-const smallGreenBoxes = Array(commits).fill(1).map(box => {
+const smallGreenBoxes = Array(commits).fill(1).map((_, i) => {
     return (
-        <div className='small-box'></div>
+        <div className='small-box'>{i + 1}</div>
     )
 })
 
-const smallGrayBoxes = Array(31 - commits).fill(1).map(box => {
+const smallGrayBoxes = Array(31 - commits).fill(1).map((_, i) => {
     return (
-        <div className='small-box gray'></div>
+        <div className='small-box gray'>{i + commits + 1}</div>
     )
 })
 
@@ -20,7 +20,7 @@ export const slides = [
                 <h2>Work: get a promotion</h2>
                 <h2>Gym: change body composition</h2>
                 <h2>Coding: make useful apps</h2>
-                <h2>Budget: track expenses</h2>
+                <h2>Budget: pay off mortgage, build retirement</h2>
             </>
         )
     },
@@ -36,17 +36,17 @@ export const slides = [
                 </div>
                 <h2>go to gym twice a week</h2>
                 <div className='container'>
+                    <div className='big-box gray yellow'>1</div>
                     <div className='big-box gray'>0</div>
-                    <div className='big-box gray'></div>
-                    <div className='big-box gray'></div>
-                    <div className='big-box gray'></div>
+                    <div className='big-box gray'>0</div>
+                    <div className='big-box gray'>0</div>
                 </div>
                 <h2>social outing each weekend?</h2>
                 <div className='container'>
                     <div className='big-box gray'>?</div>
-                    <div className='big-box gray'>~</div>
-                    <div className='big-box gray'>~</div>
-                    <div className='big-box gray'></div>
+                    <div className='big-box yellow'>~</div>
+                    <div className='big-box gray'>?</div>
+                    <div className='big-box gray'>?</div>
                 </div>
             </>
         )
