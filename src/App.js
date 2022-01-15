ae90a94964b2fcd1433fefd5dc508dff8e35ec26
crochetcode <fred.davison@outlook.com>
2022-01-14 21:26:52 -0600

    mvp

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import './App.css';
 
 function App() {
   return (
-    <div className='main'>
+    <div className='main flex-col'>
       <header className='flex'>
         <img src={logo} className="logo" alt="logo" />
         <div className='flex-col'>
           <h2>Doily.Dev</h2>
-          <button>enter</button>
+          <button>coming soon</button>
         </div>
       </header>
     </div>
