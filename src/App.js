9ac7a33e65489c5ecd4c43b4de1892b8111413d5
crochetcode <fred.davison@outlook.com>
2022-02-09 19:38:41 -0600

    add buttons

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 
 import './css/App.css';
-import './css/header.css';
-import './css/phone.css';
 import { Routes, Route } from 'react-router-dom';
 import { Home } from './pages/home';
-import { Header } from './components/header';
-import { Footer } from './components/footer';
 
 function App() {
   return (
-    <div className='main flex'>
-      <Header />
+    <div className='flex'>
       <Routes>
         <Route path='/*' element={<Home />} />
       </Routes>
-      <Footer />
     </div>
   );
 }
--- src/pages/home.js
+++ src/pages/home.js
@@ -2,8 +2,24 @@ import logo from '../assets/thread.png';
 
 export const Home = () => {
     return (
-        <div className='flex'>
-            <img src={logo} className='logo' alt='logo' />
+        <div className='flex-col main spread'>
+            <div>
+                <div>
+                    <h1>DOILY</h1>
+                </div>
+                <div className='flex spread'>
+                    <h1>DEV</h1>
+                    <img src={logo} className='logo' alt='logo' />
+                </div>
+            </div>
+            <div>
+                <a href='https://github.com/crochetcode' target='_blank' rel='noreferrer'>
+                    <button><i class="fab fa-github"></i> Github</button>
+                </a>
+                <a href='https://www.instagram.com/doily.dev' target='_blank' rel='noreferrer'>
+                    <button><i class="fab fa-instagram"></i> Instagram</button>
+                </a>
+            </div>
         </div>
     )
 }
