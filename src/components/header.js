6a471bb5fdb1573ee8164c7b5354e39370cb64d7
crochetcode <fred.davison@outlook.com>
2022-03-18 00:54:17 -0500

    change color scheme

--- src/components/header.js
+++ src/components/header.js
@@ -9,9 +9,20 @@ export const Header = () => {
             {!menu && <i className='fa-solid fa-bars' onClick={() => showMenu(true)}></i>}
             {menu && <i class='fa-solid fa-xmark' onClick={() => showMenu(false)}></i>}
             {menu && <nav className='flex-col'>
-                <div>
+                <div className='flex'>
                     <h2>👋👋 hi! 👋👋</h2>
                     <p>more coming soon</p>
+                </div>
+                <div className='flex links'>
+                    <a href='https://github.com/crochetcode' target='_blank' rel='noreferrer' className='flex'>
+                        <i class="fab fa-github"></i>
+                    </a>
+                    <a href='https://www.linkedin.com/in/fdavison1/' target='_blank' rel='noreferrer' className='flex'>
+                        <i class="fa-brands fa-linkedin-in"></i>
+                    </a>
+                    <a href='https://www.instagram.com/doily.dev' target='_blank' rel='noreferrer' className='flex'>
+                        <i class="fab fa-instagram"></i>
+                    </a>
                 </div>
             </nav>}
         </header>
