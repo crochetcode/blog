4a423250998bf8cd55063b1a068b1e6dc2352046
crochetcode <fred.davison@outlook.com>
2022-03-17 19:58:44 -0500

    header, footer

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,22 +1,30 @@
-import logo from '../assets/thread.png';
+import logo from '../assets/thread.svg';
 
 export const Home = () => {
     return (
-        <div className='flex-col main'>
-            <div className='flex-col'>
-                <div className='flex'>
-                    <h1>DOILY DEV</h1>
+        <>
+            <header className='flex'>
+                <h2>DOILY DEV</h2>
+                <i className="fa-solid fa-bars"></i>
+            </header>
+            <div className='flex-col main'>
+                <div className='flex-col'>
+                    <img src={logo} className='logo-spin' alt='logo' />
                 </div>
-                <img src={logo} className='logo' alt='logo' />
             </div>
-            <div>
-                <a href='https://github.com/crochetcode' target='_blank' rel='noreferrer'>
-                    <button><i class="fab fa-github"></i> Github</button>
-                </a>
-                <a href='https://www.instagram.com/doily.dev' target='_blank' rel='noreferrer'>
-                    <button><i class="fab fa-instagram"></i> Instagram</button>
-                </a>
-            </div>
-        </div>
+            <footer className='flex'>
+                <nav className='flex'>
+                    <a href='https://github.com/crochetcode' target='_blank' rel='noreferrer' className='flex'>
+                        <i class="fab fa-github"></i>
+                    </a>
+                    <a href='https://www.linkedin.com/in/fdavison1/' target='_blank' rel='noreferrer' className='flex'>
+                        <i class="fa-brands fa-linkedin-in"></i>
+                    </a>
+                    <a href='https://www.instagram.com/doily.dev' target='_blank' rel='noreferrer' className='flex'>
+                        <i class="fab fa-instagram"></i>
+                    </a>
+                </nav>
+            </footer>
+        </>
     )
 }
