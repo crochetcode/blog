b30f3ddc04e61e63dcda8647717f0c6dfc4b9570
crochetcode <fred.davison@outlook.com>
2022-03-17 20:29:21 -0500

    extract components

---
+++ src/components/header.js
@@ -0,0 +1,8 @@
+export const Header = () => {
+    return (
+        <header className='flex'>
+            <h2>DOILY DEV</h2>
+            <i className="fa-solid fa-bars"></i>
+        </header>
+    )
+}
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,30 +1,17 @@
 import logo from '../assets/thread.svg';
+import { Header } from '../components/header';
+import { Footer } from '../components/footer';
 
 export const Home = () => {
     return (
         <>
-            <header className='flex'>
-                <h2>DOILY DEV</h2>
-                <i className="fa-solid fa-bars"></i>
-            </header>
+            <Header />
             <div className='flex-col main'>
-                <div className='flex-col'>
+                <div className='flex'>
                     <img src={logo} className='logo-spin' alt='logo' />
                 </div>
             </div>
-            <footer className='flex'>
-                <nav className='flex'>
-                    <a href='https://github.com/crochetcode' target='_blank' rel='noreferrer' className='flex'>
-                        <i class="fab fa-github"></i>
-                    </a>
-                    <a href='https://www.linkedin.com/in/fdavison1/' target='_blank' rel='noreferrer' className='flex'>
-                        <i class="fa-brands fa-linkedin-in"></i>
-                    </a>
-                    <a href='https://www.instagram.com/doily.dev' target='_blank' rel='noreferrer' className='flex'>
-                        <i class="fab fa-instagram"></i>
-                    </a>
-                </nav>
-            </footer>
+            <Footer />
         </>
     )
 }
