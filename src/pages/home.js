3cbf8d20dc57f8e58300c1d0007ffa82502d1315
crochetcode <fred.davison@outlook.com>
2022-03-19 10:58:18 -0500

    add headline

--- src/pages/home.js
+++ src/pages/home.js
@@ -7,9 +7,11 @@ export const Home = () => {
             <Header />
             <main>
                 <div className='flex-col splash'>
-                    <div className='flex'>
-                        <img src={logo} className='logo-spin' alt='logo' />
+                    <div className='flex-col words'>
+                        {/* <h1>DOILY DEV</h1> */}
+                        <h1>personal blog of a junior developer</h1>
                     </div>
+                    <img src={logo} className='logo-spin' alt='logo' />
                 </div>
             </main>
         </>
