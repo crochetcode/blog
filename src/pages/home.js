2552e351f794c22a18c39f28964da63825f8177f
crochetcode <fred.davison@outlook.com>
2022-03-19 10:59:24 -0500

    comments

--- src/pages/home.js
+++ src/pages/home.js
@@ -8,7 +8,6 @@ export const Home = () => {
             <main>
                 <div className='flex-col splash'>
                     <div className='flex-col words'>
-                        {/* <h1>DOILY DEV</h1> */}
                         <h1>personal blog of a junior developer</h1>
                     </div>
                     <img src={logo} className='logo-spin' alt='logo' />
