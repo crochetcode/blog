1414f8e31b0aefbcb1c420bb5a1b5c1c780dcc1a
crochetcode <fred.davison@outlook.com>
2022-03-26 19:28:28 -0500

    post order

--- src/constants/post-previews.js
+++ src/constants/post-previews.js
@@ -3,17 +3,17 @@ import happyDev from '../assets/post-imgs/happy-dev.jpeg';
 
 export const postPreviews = [
     {
-        link: '/about',
-        h2: 'about',
-        p: 'Hi! I\'m Fred!',
-        img: happyDev,
-        date: 'March 25th, 2022'
-    },
-    {
         link: '/blog/stack',
         h2: 'stack and tools',
         p: 'What I used to make this site',
         img: react,
         date: 'March 26th, 2022'
     },
+    {
+        link: '/about',
+        h2: 'about',
+        p: 'Hi! I\'m Fred!',
+        img: happyDev,
+        date: 'March 25th, 2022'
+    }
 ]
